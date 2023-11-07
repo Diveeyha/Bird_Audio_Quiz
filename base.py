0a7b2eb361a2b0e307a68d7aece9be1b92bfa795
Diveeyha <67159522+Diveeyha@users.noreply.github.com>
2023-11-06 19:44:42 -0500

    Update base.py

--- base.py
+++ base.py
@@ -1,7 +1,7 @@
 import streamlit as st
-import random
 from birds.database import load_csv, get_birds_by_family
-from birds.audio import find_bird_urls
+from birds.audio import get_audio
+from birds.image import get_image, get_caption
 
 
 def initialize_session_state():
@@ -13,10 +13,19 @@ def initialize_session_state():
         st.session_state.question_counter = 0
     if 'previous_answer' not in st.session_state:
         st.session_state.previous_answer = ""
-        
+    if 'correct_answer' not in st.session_state:
+        st.session_state.correct_answer = ""
+
+
+def calculate_score(guess, answer):
+    if guess == answer:
+        st.session_state.player_score += 1
+    st.session_state.question_counter += 1
+
 
 def clear_select():
-    st.session_state.my_select = None
+    calculate_score(st.session_state.player_choice, st.session_state.correct_answer)
+    st.session_state.player_choice = None
 
 
 def reset_session_state():
@@ -32,77 +41,72 @@ def bird_data(bird_filter):
     return birds
 
 
-def get_audio(question_number, birds, answer):
-    audio_file = find_bird_urls(birds['name'])
-    url = random.choice(audio_file[answer])
-    return url
-
-
 def data_filter():
-    if st.session_state.txt_filter == "Test":
+    if st.session_state.filter_select == "Test":
         bird_filter = get_birds_by_family('sulidae')
         return bird_filter
-    elif st.session_state.txt_filter == "All":
+    elif st.session_state.filter_select == "All":
         bird_filter = load_csv()
         return bird_filter
-    elif st.session_state.txt_filter == "Waterfowl":
+    elif st.session_state.filter_select == "Waterfowl":
         bird_filter = get_birds_by_family('anatidae')
         return bird_filter
-
-
-def calculate_score(player_choice, correct_answer):
-    if player_choice == correct_answer:
-        st.session_state.player_score += 1
-    st.session_state.question_counter += 1
 
 
 st.title("North American Bird Quiz")
 initialize_session_state()
 
-tab1, tab2 = st.tabs(["Bird List", "Audio"])
-with tab1:
-    st.radio("Filter by", ["Test", "All", "Waterfowl"], horizontal=True, key="txt_filter")
-    st.dataframe(data_filter(), hide_index=True)
+with st.sidebar:
+    st.radio("Quiz", ["Audio Only", "Image & Audio"], horizontal=True, key="quiz_radio")
+    st.selectbox("Filter by", ("Test", "All", "Waterfowl"), key="filter_select")
+    st.dataframe(data_filter()['name'], hide_index=True)
 
-with tab2:
-    birds = bird_data(data_filter())
-    ind = st.session_state.question_number
-    options = birds['name'].sort_values()
-    answer = birds.iloc[ind, 0]
-    st.audio(get_audio(ind, birds, answer))
-    # st.write(ind, answer)
 
-    guess = st.selectbox("Answer:", options, key="my_select", index=None)
+st.caption("This quiz tests your identification skills.")
+birds = bird_data(data_filter())
+ind = st.session_state.question_number
+options = birds['name'].sort_values()
+st.session_state.correct_answer = birds.iloc[ind, 0]
 
-    col1, col2 = st.columns(2)
+image_url = get_image(birds, st.session_state.correct_answer)
+#caption_url = get_caption(birds, st.session_state.correct_answer)
+if (st.session_state.quiz_radio == "Image & Audio"):
+    st.image(image_url, width=470)
+
+st.audio(get_audio(birds, st.session_state.correct_answer))
+# st.write(ind, st.session_state.correct_answer)
+
+with st.form(key="user_guess"):
+    st.selectbox("Answer:", options, key="player_choice", index=None)
+
+    col1, col2 = st.columns([8, 1])
     with col1:
-        if st.button("Submit", key="submit", on_click=clear_select):
-            calculate_score(guess, answer)
+        if st.form_submit_button("Submit", on_click=clear_select):
 
-            if st.session_state.question_number == (len(birds)-1):
-                st.session_state.previous_answer = answer
+            if st.session_state.question_number == (len(birds) - 1):
+                st.session_state.previous_answer = st.session_state.correct_answer
                 reset_session_state()
 
             elif st.session_state.question_number < len(birds):
                 idx = st.session_state.question_number
                 idx = (idx + 1) % len(birds)
                 st.session_state.question_number = idx
-                st.session_state.previous_answer = answer
+                st.session_state.previous_answer = st.session_state.correct_answer
                 st.experimental_rerun()
 
-
-    if col2.button("Reset", key="reset", type="primary", on_click=clear_select):
-        st.session_state.player_score = 0
-        st.session_state.question_counter = 0
-        st.session_state.previous_answer = ""
-        reset_session_state()
-
-    col1, col2 = st.columns(2)
-    with col1:
-        if st.session_state.question_counter > 0:
-            st.write(f"The correct answer is {st.session_state.previous_answer}.")
     with col2:
-        if st.session_state.question_counter > 0:
-            st.write(f"Score: {st.session_state.player_score} correct out of {st.session_state.question_counter}.")
+        if st.form_submit_button("Reset", type="primary", on_click=clear_select):
+            st.session_state.player_score = 0
+            st.session_state.question_counter = 0
+            st.session_state.previous_answer = ""
+            reset_session_state()
 
-#st.session_state
+col1, col2 = st.columns(2)
+with col1:
+    if st.session_state.question_counter > 0:
+        st.write(f"Correct answer is {st.session_state.previous_answer}.")
+with col2:
+    if st.session_state.question_counter > 0:
+        st.write(f"Score: {st.session_state.player_score} correct out of {st.session_state.question_counter}.")
+
+st.session_state
