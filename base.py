cc60f75464f7a0a9fa7e2b08350990d8cc3a1288
Diveeyha <67159522+Diveeyha@users.noreply.github.com>
2023-11-16 16:52:45 -0500

    Update base.py

--- base.py
+++ base.py
@@ -1,8 +1,9 @@
 import streamlit as st
-from birds.database import (load_csv, get_birds_by_group,
+from birds.database import (load_csv, get_birds_by_group, get_birds_by_name,
                             get_birds_by_order, get_birds_by_family)
 from birds.audio import get_audio
 from birds.image import get_image
+from birds.utils import clean_bird_name
 
 
 def initialize_session_state():
@@ -46,39 +47,67 @@ def reset_session_state():
 @st.cache_data
 def bird_data(bird_filter):
     random_birds = bird_filter.sample(frac=1)
-    # st.data_editor(birds)
     return random_birds
 
 
-def data_filter():
-    bird_csv = load_csv()
+def data_filter(state):
+    if state == 'All':
+        bird_csv = load_csv('database.csv')
+    else:
+        bird_csv = load_csv('state_info.csv')
+        bird_csv = bird_csv[bird_csv[state] == 1]
+    st.selectbox("Select", ("All", "Group", "Order", "Family", "Species"), key="filter_select")
     if st.session_state.filter_select == "All":
         bird_filter = bird_csv
         return bird_filter
     elif st.session_state.filter_select == "Group":
         selections = bird_csv['group'].unique()
         st.multiselect('Groups', list(selections), key="multi_group")
-        bird_filter = get_birds_by_group(st.session_state.multi_group)
+        bird_filter = get_birds_by_group(bird_csv, st.session_state.multi_group)
+        df_birds = bird_csv['name'].unique()
         if len(st.session_state.multi_group) >= 1:
             return bird_filter
         else:
+            st.dataframe(df_birds, hide_index=True, use_container_width=True)
             st.stop()
     elif st.session_state.filter_select == "Order":
         selections = bird_csv['order'].unique()
         st.multiselect('Orders', list(selections), key="multi_order")
-        bird_filter = get_birds_by_order(st.session_state.multi_order)
+        bird_filter = get_birds_by_order(bird_csv, st.session_state.multi_order)
+        df_birds = bird_csv['name'].unique()
         if len(st.session_state.multi_order) >= 1:
             return bird_filter
         else:
+            st.dataframe(df_birds, hide_index=True, use_container_width=True)
             st.stop()
     elif st.session_state.filter_select == "Family":
         selections = bird_csv['family'].unique()
         st.multiselect('Families', list(selections), key="multi_family")
-        bird_filter = get_birds_by_family(st.session_state.multi_family)
+        bird_filter = get_birds_by_family(bird_csv, st.session_state.multi_family)
+        df_birds = bird_csv['name'].unique()
         if len(st.session_state.multi_family) >= 1:
             return bird_filter
         else:
+            st.dataframe(df_birds, hide_index=True, use_container_width=True)
             st.stop()
+    elif st.session_state.filter_select == "Species":
+        selections = bird_csv['name'].unique()
+        st.multiselect('Species', list(selections), key="multi_species")
+        bird_filter = get_birds_by_name(bird_csv, st.session_state.multi_species)
+        df_birds = bird_csv['name'].unique()
+
+        if len(st.session_state.multi_species) > 1:
+            return bird_filter
+        else:
+            st.dataframe(df_birds, hide_index=True, use_container_width=True)
+            st.stop()
+
+
+def area_filter():
+    state_options = load_csv('state_codes.csv')
+    state_options.loc[0] = 'All'
+    st.selectbox("You may choose to filter by State", state_options, key="filter_state")
+    return bird_data(data_filter(st.session_state.filter_state))
 
 
 st.title("North American Bird Quiz")
@@ -87,13 +116,15 @@ initialize_session_state()
 
 with st.sidebar:
     st.radio("Quiz", ["Audio Only", "Image & Audio"], horizontal=True, key="quiz_radio")
-    st.selectbox("Select", ("All", "Group", "Order", "Family"), key="filter_select")
-    birds = bird_data(data_filter())
+    #
+    birds = area_filter()
+
     options = birds['name'].sort_values()
     st.dataframe(options, hide_index=True, use_container_width=True)
 
 ind = st.session_state.question_number
 st.session_state.correct_answer = birds.iloc[ind, 0]
+answer_options = birds.loc[birds['quiz_answer_groups'] == birds.iloc[ind, 5]]
 
 image_url, caption_url = get_image(birds, st.session_state.correct_answer)
 if st.session_state.quiz_radio == "Image & Audio":
@@ -102,7 +133,7 @@ if st.session_state.quiz_radio == "Image & Audio":
 st.audio(get_audio(birds, st.session_state.correct_answer))
 
 with st.form(key="user_guess"):
-    st.selectbox("Answer:", options, key="player_choice", index=None)
+    st.selectbox("Answer:", answer_options['name'].sort_values(), key="player_choice", index=None)
 
     col1, col2 = st.columns([8, 1])
     with col1:
@@ -129,10 +160,11 @@ with st.form(key="user_guess"):
 col1, col2 = st.columns(2)
 with col1:
     if st.session_state.question_counter > 0:
-        st.write(f"Correct answer is {st.session_state.previous_answer}.")
+        formatted_birdname = clean_bird_name(st.session_state.previous_answer)
+        st.write(f"Correct answer is [{st.session_state.previous_answer}](https://www.allaboutbirds.org/guide/{formatted_birdname}).")
 with col2:
     if st.session_state.question_counter > 0:
         st.write(f"Score: {st.session_state.player_score} correct out of {st.session_state.question_counter}.")
 
 st.caption("All media sourced from [The Cornell Lab of Ornithology: Macaulay Library](https://www.macaulaylibrary.org)")
-st.session_state
+# st.session_state
