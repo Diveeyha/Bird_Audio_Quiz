5caef19cbdcd3319be4f735e9d41c941939652f5
Diveeyha <67159522+Diveeyha@users.noreply.github.com>
2023-11-22 14:05:48 -0500

    Update base.py

--- base.py
+++ base.py
@@ -4,7 +4,6 @@ from birds.database import (load_csv, get_birds_by_group, get_birds_by_taxonomic
 from birds.audio import get_audio
 from birds.image import get_image
 from birds.utils import clean_bird_name
-from streamlit.delta_generator import DeltaGenerator
 
 
 def initialize_session_state():
@@ -54,6 +53,9 @@ def bird_data(bird_filter):
 
 
 def filter_selections(bird_csv, user_input):
+    test_err = ('''Quiz will automatically load when minimum requirements are met.\n
+                Please make sure that more than one species is included in the filter.''')
+
     lookup_dict = {'group': get_birds_by_group, 'order': get_birds_by_order,
                    'family': get_birds_by_family, 'species': get_birds_by_taxonomic_name}
     key_dict = {'multi_group': st.session_state.multi_group, 'multi_order': st.session_state.multi_order,
@@ -65,30 +67,27 @@ def filter_selections(bird_csv, user_input):
     selections = bird_csv[user_input_lower].unique()
     selections = [names.lower() for names in selections]
     selections = sorted(selections)
-    st.multiselect(user_input, list(selections), key=key_var)
+
+    st.sidebar.multiselect(user_input, list(selections), key=key_var, label_visibility="collapsed")
 
     get_birds_func_call = lookup_dict[user_input_lower]
     bird_filter = get_birds_func_call(bird_csv, key_dict[key_var])
     df_birds = bird_filter['name'].unique()
+
     if len(key_dict[key_var]) >= 1:
         if len(bird_filter) > 1:
             return bird_filter
         else:
-            st.toast("Choose an additional species.", icon='😍')
-            st.dataframe(df_birds, hide_index=True, use_container_width=True)
-            st.stop()
+            st.sidebar.dataframe(df_birds, hide_index=True, use_container_width=True)
+            st.divider()
+            return st.error(test_err), st.stop()
     else:
-        st.dataframe(df_birds, hide_index=True, use_container_width=True)
-        st.stop()
+        st.sidebar.dataframe(df_birds, hide_index=True, use_container_width=True)
+        st.divider()
+        return st.error(test_err), st.stop()
 
 
-def data_filter(state):
-    if state == 'All':
-        bird_csv = load_csv('state_info.csv')
-    else:
-        bird_csv = load_csv('state_info.csv')
-        bird_csv = bird_csv[bird_csv[state.lower()] == 1]
-    st.radio("Filter by: ", ["All", "Group", "Order", "Family", "Species"], horizontal=True, key="filter_select")
+def data_filter(bird_csv):
     if st.session_state.filter_select == "All":
         bird_filter = bird_csv
         return bird_filter
@@ -96,13 +95,21 @@ def data_filter(state):
         return filter_selections(bird_csv, st.session_state.filter_select)
 
 
-def area_filter():
+def state_filter(state):
+    if state == 'All':
+        bird_csv = load_csv('state_info.csv')
+    else:
+        bird_csv = load_csv('state_info.csv')
+        bird_csv = bird_csv[bird_csv[state.lower()] == 1]
+    return bird_csv
+
+
+def state_dropdown_options():
     state_options = load_csv('state_codes.csv')
     state_options.loc[-1] = ['All']
     state_options.index = state_options.index + 1  # shifting index
     state_options.sort_index(inplace=True)
-    st.selectbox("Filter by State:", state_options, key="filter_state")
-    return bird_data(data_filter(st.session_state.filter_state))
+    return state_options
 
 
 def main():
@@ -111,11 +118,17 @@ def main():
     st.write("This quiz tests your identification skills.")
     initialize_session_state()
 
-    with st.sidebar:
-        st.radio("Quiz", ["Audio Only", "Image & Audio"], horizontal=True, key="quiz_radio")
-        birds = area_filter()
-        options = birds['name'].sort_values()
-        st.dataframe(options, hide_index=True, use_container_width=True)
+    st.sidebar.radio("Quiz", ["Audio Only", "Image & Audio"], horizontal=True, key="quiz_radio")
+    st.sidebar.divider()
+    st.sidebar.selectbox("Filter by State:", state_dropdown_options(), key="filter_state")
+    st.sidebar.divider()
+    st.sidebar.radio("Filter by: ", ["All", "Group", "Order", "Family", "Species"], horizontal=True,
+                     key="filter_select")
+    birds = bird_data(data_filter(state_filter(st.session_state.filter_state)))
+    options = birds['name'].sort_values()
+    st.sidebar.divider()
+    st.sidebar.dataframe(options, hide_index=True, use_container_width=True)
+
 
     ind = st.session_state.question_number
     st.session_state.correct_answer = birds.iloc[ind, 0]
@@ -123,16 +136,18 @@ def main():
 
     image_url, caption_url = get_image(birds, st.session_state.correct_answer)
     if st.session_state.quiz_radio == "Image & Audio":
-        adj1, adj2, adj3 = st.columns([1, 2, 1])
+        adj1, adj2, adj3 = st.columns([1, 4, 1])
         adj2.image(image_url, caption_url, use_column_width="always")
 
     st.audio(get_audio(birds, st.session_state.correct_answer))
 
     with st.form(key="user_guess"):
-        st.selectbox("Answer:", answer_options['name'].sort_values(), key="player_choice", index=None)
 
-        col1, col2 = st.columns(2)
-        with col1:
+        col1, empty, col2, col3 = st.columns([2, 0.75, 0.75, 0.75])
+        col1.selectbox("Answer:", answer_options['name'].sort_values(), key="player_choice",
+                       index=None, label_visibility="collapsed")
+
+        with col2:
             if st.form_submit_button("Submit", on_click=clear_select):
 
                 if st.session_state.question_number == (len(birds) - 1):
@@ -146,29 +161,33 @@ def main():
                     st.session_state.previous_answer = st.session_state.correct_answer
                     st.experimental_rerun()
 
-        with col2:
+        with col3:
             if st.form_submit_button("Reset", type="primary", on_click=clear_select):
                 st.session_state.player_score = 0
                 st.session_state.question_counter = 0
                 st.session_state.previous_answer = ""
                 reset_session_state()
 
-    col1, col2 = st.columns(2)
-    with col1:
+    print_col1, print_col2 = st.columns(2)
+    with print_col1:
         if st.session_state.question_counter > 0:
             formatted_name = clean_bird_name(st.session_state.previous_answer)
             st.write(f"Correct answer is [{st.session_state.previous_answer}]"
                      f"(https://www.allaboutbirds.org/guide/{formatted_name}).")
-    with col2:
+    with print_col2:
         if st.session_state.question_counter > 0:
             st.write(f"Score: {st.session_state.player_score} correct out of {st.session_state.question_counter}.")
 
+    st.divider()
     st.caption(f"All media sourced from [The Cornell Lab of Ornithology: Macaulay Library]"
-                           f"(https://www.macaulaylibrary.org)")
+               f"(https://www.macaulaylibrary.org)")
 # st.session_state
 
 
 # Run main
 if __name__ == "__main__":
-    st.markdown("<style>body {text-align: center}</style>", unsafe_allow_html=True)
+    st.markdown("""<style>body {text-align: center}
+                p {text-align: center} 
+                button {float: center} </style>""", unsafe_allow_html=True)
+    # st.markdown("<style></style>", unsafe_allow_html=True)
     main()
